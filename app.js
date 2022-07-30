d045210dae0257b778558626f6635c2c44f5195b
puneetbhatnagarr <punitrock11@gmail.com>
2022-07-30 12:16:50 +0530

    ss

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 const port = process.env.PORT || 3000;
+var cors = require('cors')
+app.use(cors());
 var bodyParser = require('body-parser')
 const {connectdb} = require('./Db/connect_db.js')
 const web = require('./routes/web.js');
-const cors = require('cors')
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
