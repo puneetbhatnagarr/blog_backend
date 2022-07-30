f26bd8c6b578b98051e4f1915ca795c9216ef2ec
puneetbhatnagarr <punitrock11@gmail.com>
2022-07-30 11:27:05 +0530

    ssd

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ var bodyParser = require('body-parser')
 const {Router} = require('express')
 const {connectdb} = require('./Db/connect_db.js')
 const web = require('./routes/web.js');
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+    next();
+  });
 var cors = require('cors')
- 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
