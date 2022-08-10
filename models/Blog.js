69aaf92c88b4675dbe9aa65a5fe87689024c488d
puneetbhatnagarr <punitrock11@gmail.com>
2022-08-10 09:40:04 +0530

    aa

--- models/Blog.js
+++ models/Blog.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const newSchema = new mongoose.Schema({
     title:{type:String,required:true},
     description:{type:String,required:true},
-    body:{type:String,required: true}
+    body:{type:String,required: true},
+    image:{type:String,required:true}
 },
 {timestamps:true});
 
